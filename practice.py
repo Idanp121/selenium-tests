b0e55b67f03f0d036dda4c7ae69df170556b7002
Idan Peretz <idanp809@gmail.com>
2025-06-09 00:46:10 +0300

    תרגול פקודות למתחילים

---
+++ practice.py
@@ -0,0 +1,10 @@
+# בנה תוכנית שמבקשת מהמשתמש:
+# את השם שלו
+# את הגיל שלו
+# ולאחר מכן:
+# תחשב בעוד כמה שנים המשתמש יהיה בן 100
+# תדפיס משפט מסכם עם השם והחישוב
+# name = input("Enter your name" + " ")
+# age = int(input("enter your age" + " "))
+# x = (100 - age)
+# print('your name is ' + name + " mor " + str(x) + ' year you will be 100 age')
